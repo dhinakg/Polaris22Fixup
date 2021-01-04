c2eeab7da2f45a7ed12af83bd7c5ee5937382e97
Dhinak G <dhinak2004@gmail.com>
2021-01-04 10:00:21 -0500

    Initial testing

--- Polaris22Fixup/kern_start.cpp
+++ Polaris22Fixup/kern_start.cpp
@@ -9,7 +9,7 @@
 #include <Headers/kern_api.hpp>
 
 #define UNLIKELY(x) __builtin_expect(!!(x), 0)
-#define MODULE_SHORT "p22"
+#define MODULE_SHORT "sidecar"
 
 extern "C" void *memmem(const void *h0, size_t k, const void *n0, size_t l);
 
@@ -17,21 +17,15 @@ static const int kPathMaxLen = 1024;
 
 #pragma mark - Patches
 
-static const int kEllesmereDeviceId = 0x67DF;
+static const uint8_t kMacModelOriginal[] = "MacPro6,1";
 
-static const uint8_t kAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal[] = {
-    0xb8, 0x02, 0x00, 0x00, 0x00, 0x0f, 0x43, 0xc1, 0xeb,
-};
+static const uint8_t kMacModelPatched[] = "MacPro7,1";
 
-static const uint8_t kAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched[] = {
-    0xb8, 0x02, 0x00, 0x00, 0x00, 0x90, 0x90, 0x90, 0xeb,
-};
+static constexpr size_t kMacModelOriginalSize = sizeof(kMacModelOriginal);
 
-static constexpr size_t kAmdBronzeMtlAddrLibGetBaseArrayModeReturnSize = sizeof(kAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal);
+static_assert(kMacModelOriginalSize == sizeof(kMacModelPatched), "patch size invalid");
 
-static_assert(kAmdBronzeMtlAddrLibGetBaseArrayModeReturnSize == sizeof(kAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched), "patch size invalid");
-
-static const uint8_t kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal[] = {
+/* static const uint8_t kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal[] = {
     0xb9, 0x02, 0x00, 0x00, 0x00, 0x01, 0xc8, 0x41, 0x83, 0xf8, 0x21, 0x0f, 0x42, 0xc1, 0xeb,
 };
 
@@ -41,9 +35,9 @@ static const uint8_t kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched[] =
 
 static constexpr size_t kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnSize = sizeof(kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal);
 
-static_assert(kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnSize == sizeof(kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched), "patch size invalid");
+static_assert(kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnSize == sizeof(kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched), "patch size invalid"); */
 
-static const char kAmdBronzeMtlDriverPath[kPathMaxLen] = "/System/Library/Extensions/AMDMTLBronzeDriver.bundle/Contents/MacOS/AMDMTLBronzeDriver";
+static const char kSidecarCorePath[kPathMaxLen] = "/System/Library/PrivateFrameworks/SidecarCore.framework/Versions/A/SidecarCore";
 
 static const char kDyldCachePath[kPathMaxLen] = "/private/var/db/dyld/dyld_shared_cache_x86_64h";
 
@@ -80,7 +74,7 @@ static inline void searchAndPatch(const void *haystack,
                                   const char (&dylibCachePath)[kPathMaxLen],
                                   const uint8_t (&needle)[patchSize],
                                   const uint8_t (&patch)[patchSize]) {
-    if (UNLIKELY(strncmp(path, kAmdBronzeMtlDriverPath, sizeof(kAmdBronzeMtlDriverPath)) == 0) ||
+    if (UNLIKELY(strncmp(path, kSidecarCorePath, sizeof(kSidecarCorePath)) == 0) ||
         UNLIKELY(strncmp(path, dylibCachePath, sizeof(dylibCachePath)) == 0)) {
         void *res;
         if (UNLIKELY((res = memmem(haystack, haystackSize, needle, patchSize)) != NULL)) {
@@ -106,7 +100,7 @@ static boolean_t patched_cs_validate_range(vnode_t vp,
     int pathlen = kPathMaxLen;
     boolean_t res = FunctionCast(patched_cs_validate_range, orig_cs_validate)(vp, pager, offset, data, size, result);
     if (res && vn_getpath(vp, path, &pathlen) == 0) {
-        searchAndPatch(data, size, path, kDyldCachePath, kAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal, kAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched);
+        searchAndPatch(data, size, path, kDyldCachePath, kMacModelOriginal, kMacModelPatched);
     }
     return res;
 }
@@ -123,7 +117,7 @@ static void patched_cs_validate_page(vnode_t vp,
     int pathlen = kPathMaxLen;
     FunctionCast(patched_cs_validate_page, orig_cs_validate)(vp, pager, page_offset, data, arg4, arg5, arg6);
     if (vn_getpath(vp, path, &pathlen) == 0) {
-        searchAndPatch(data, PAGE_SIZE, path, kBigSurDyldCachePath, kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnOriginal, kBigSurAmdBronzeMtlAddrLibGetBaseArrayModeReturnPatched);
+        searchAndPatch(data, PAGE_SIZE, path, kDyldCachePath, kMacModelOriginal, kMacModelPatched);
     }
 }
 
@@ -195,7 +189,7 @@ PluginConfiguration ADDPR(config) {
     arrsize(bootargDebug),
     bootargBeta,
     arrsize(bootargBeta),
-    KernelVersion::Mojave,
+    KernelVersion::Catalina,
     KernelVersion::BigSur,
     pluginStart
 };
